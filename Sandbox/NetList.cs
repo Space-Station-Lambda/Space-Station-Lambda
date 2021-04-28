950a6085714d5026fd4b732655a8e4bc07082844
Lévy Marques <prakkmak@gmail.com>
2021-04-28 17:08:23 +0200

    Ajout d'interfaces sandbox

---
+++ Sandbox/NetList.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace Sandbox
+{
+    public interface NetList<T>
+    {
+        void Clear();
+        void Get(Type type);
+        void Set(Type type, T value);
+    }
+}
