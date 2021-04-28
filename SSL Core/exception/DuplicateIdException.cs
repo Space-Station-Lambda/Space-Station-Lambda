809dd224293b81b573339a11af1fb91d9bed4353
Lévy Marques <prakkmak@gmail.com>
2021-04-28 16:48:15 +0200

    Quelques ajouts d'exceptions pour les objets

---
+++ SSL Core/exception/DuplicateIdException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SSL_Core.exception
+{
+    /// <summary>
+    /// Deux identifiants uniques sont égaux; i.e utilisation d'un Id d'Item déjà utilisé
+    /// </summary>
+    public class DuplicateIdException : Exception
+    {
+        public DuplicateIdException() {}   
+        public DuplicateIdException(string message) : base(message) { }
+        public DuplicateIdException(string message, Exception inner) : base(message, inner) { }
+        protected DuplicateIdException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
