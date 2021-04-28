74e2d40306b3c6c9042a62642724b660f88ac624
Lévy Marques <prakkmak@gmail.com>
2021-04-28 14:00:11 +0200

    Mise en place du socle de tests

---
+++ SSL Core Tests/model/InventoryTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace SSL_Core_Tests.model
+{
+    public class InventoryTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
