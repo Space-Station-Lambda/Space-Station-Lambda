3fbd1994d6c4c654c33910c0e4590bd586f060a0
Lévy Marques <prakkmak@gmail.com>
2021-04-28 21:23:26 +0200

    refacto items

---
+++ SSL Core/model/items/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SSL_Core.model.items
+{
+    public abstract class Item
+    {
+        public string Id { get; } // APPLE
+        public string Name { get; } // Apple
+        public int MaxStack { get; } // 100 ?
+        
+        public Item(string id, string name, int maxStack = 1)
+        {
+            Id = id;
+            Name = name;
+            MaxStack = maxStack;
+        }
+
+        /// <summary>
+        /// Utilise l'objet 
+        /// </summary>
+        public abstract void Use();
+    }
+}
