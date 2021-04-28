18432e046fb24faacf0307a9bf15b89cf0b6c0f2
Noé Pécault <noepec1@gmail.com>
2021-04-28 23:35:44 +0200

    Rename NegativeItemSlotException en NegativeStackItemException

---
+++ SSL Core/exception/NegativeStackItemException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SSL_Core.exception
+{
+    /// <summary>
+    /// Stack d'item négatif
+    /// </summary>
+    public class NegativeStackItemException : Exception
+    {
+        public NegativeStackItemException() {}   
+        public NegativeStackItemException(string message) : base(message) { }
+        public NegativeStackItemException(string message, Exception inner) : base(message, inner) { }
+        protected NegativeStackItemException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
