98f14be1da184967c3ceb71bd6d669129487219c
Noé Pécault <noepec1@gmail.com>
2021-06-24 00:46:58 +0200

    docs: Added docs on IItemUser interface

--- SSL Core/item/IItemUser.cs
+++ SSL Core/item/IItemUser.cs
@@ -2,6 +2,9 @@ using SSL_Core.item.items;
 
 namespace SSL_Core.item
 {
+    /// <summary>
+    /// Interface that should be implemented on all entities that is able to use Items
+    /// </summary>
     public interface IItemUser
     {
         //TODO: Add info on item use (position of use, target entity, ...)
