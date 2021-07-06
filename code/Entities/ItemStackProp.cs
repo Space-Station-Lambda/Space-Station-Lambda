2d9a2cb26b9aceb72b96d6002a10260169cc8de9
Noé Pécault <noepec1@gmail.com>
2021-07-06 04:05:49 +0200

    feat: Added ItemStackProp for in-world spawn of ItemStack

---
+++ code/Entities/ItemStackProp.cs
@@ -0,0 +1,40 @@
+﻿using Sandbox;
+using ssl.Item;
+
+namespace ssl.Entities
+{
+    /// <summary>
+    /// Prop of an ItemStack spawned when an item is in the world.
+    /// For example when a player drops an item from their inventory.
+    /// </summary>
+    public class ItemStackProp : Prop
+    {
+        public sealed override Rotation Rotation
+        {
+            get => base.Rotation;
+            set => base.Rotation = value;
+        }
+        public sealed override Vector3 Position
+        {
+            get => base.Position;
+            set => base.Position = value;
+        }
+        public ItemStack ItemStack { get; private set; }
+
+        public ItemStackProp()
+        {
+            
+        }
+        
+        public ItemStackProp(ItemStack itemStack, Vector3 position, Rotation rotation)
+        {
+            ItemStack = itemStack;
+            Position = position;
+            Rotation = rotation;
+            
+            SetModel(ItemStack.Item.Model);
+            SetupPhysicsFromModel(PhysicsMotionType.Dynamic);
+        }
+
+    }
+}
