9f4eb91dd242e28c85aecaf1f43048281200dc6f
Noé Pécault <noepec1@gmail.com>
2021-07-06 15:46:21 +0200

    refactor: Changed ItemStackProp constructor
    - Removed Position and Rotation
    - Extracted model methods

--- code/Entities/ItemStackProp.cs
+++ code/Entities/ItemStackProp.cs
@@ -7,34 +7,22 @@ namespace ssl.Entities
     /// Prop of an ItemStack spawned when an item is in the world.
     /// For example when a player drops an item from their inventory.
     /// </summary>
-    public class ItemStackProp : Prop
+    public partial class ItemStackProp : Prop
     {
-        public sealed override Rotation Rotation
-        {
-            get => base.Rotation;
-            set => base.Rotation = value;
-        }
-        public sealed override Vector3 Position
-        {
-            get => base.Position;
-            set => base.Position = value;
-        }
         public ItemStack ItemStack { get; private set; }
 
-        public ItemStackProp()
-        {
-            
-        }
+        public ItemStackProp() { }
         
-        public ItemStackProp(ItemStack itemStack, Vector3 position, Rotation rotation)
+        public ItemStackProp(ItemStack itemStack) : this()
         {
             ItemStack = itemStack;
-            Position = position;
-            Rotation = rotation;
-            
+            SetupModel();
+        }
+
+        protected void SetupModel()
+        {
             SetModel(ItemStack.Item.Model);
             SetupPhysicsFromModel(PhysicsMotionType.Dynamic);
         }
-
     }
 }
