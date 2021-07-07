02b2a1d7b51a5a7b46c0e4d97c3a59a07378bb41
Lévy Marques <prakkmak@gmail.com>
2021-07-07 21:44:43 +0200

    feat: add  Nauwp suggestions (#78)

--- code/Player/Clothes.cs
+++ code/Player/Clothes.cs
@@ -4,16 +4,20 @@ namespace ssl.Player
 {
     public class Clothes : ModelEntity
     {
-        public Clothes()
-        {
-        }
+        public Clothes() { }
 
         public Clothes(string clothesModel)
         {
             EnableShadowInFirstPerson = true;
             EnableHideInFirstPerson = true;
+            SetupModel(clothesModel);
+        }
+
+        private void SetupModel(string clothesModel)
+        {
             AddCollisionLayer(CollisionLayer.Debris);
             SetModel(clothesModel);
         }
+        
     }
 }
--- code/Player/ClothesHandler.cs
+++ code/Player/ClothesHandler.cs
@@ -1,40 +1,71 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Sandbox;
 
 namespace ssl.Player
 {
+    /// <summary>
+    /// Manage the clothing system for the given player.
+    /// </summary>
     public class ClothesHandler
     {
-        private readonly List<ModelEntity> clothes = new();
+        /// <summary>
+        /// List of current clothes.
+        /// </summary>
+        private readonly List<ModelEntity> clothesList = new();
+        /// <summary>
+        /// Player concerned.
+        /// </summary>
         private readonly MainPlayer player;
 
         public ClothesHandler(MainPlayer player)
         {
             this.player = player;
         }
-
+        /// <summary>
+        /// Attach a set of clothes to the player.
+        /// </summary>
+        /// <param name="clothesSet">Set of string clothes.</param>
+        /// <param name="strip">If true, strip the player before add clothes.</param>
         public void AttachClothes(IEnumerable<string> clothesSet, bool strip = true)
         {
             if (strip) Strip();
-            foreach (Clothes clothesPart in clothesSet.Select(c => new Clothes(c)))
+            foreach (string c in clothesSet)
             {
-                clothesPart.SetParent(player, true);
-                clothes.Add(clothesPart);
+                AttachClothes(c, false);
             }
         }
-
+        /// <summary>
+        /// Attach a piece of clothes to the player
+        /// </summary>
+        /// <param name="clothesPart">Name of the clothes</param>
+        /// <param name="strip">If true, strip the player before add clothes</param>
+        public void AttachClothes(string clothes, bool strip = true)
+        {
+            AttachClothes(new Clothes(clothes), false);
+        }
+        /// <summary>
+        /// Attach a piece of clothes to the player
+        /// </summary>
+        /// <param name="clothesPart">Concerned clothes</param>
+        /// <param name="strip">If true, strip the player before add clothes</param>
+        public void AttachClothes(Clothes clothes, bool strip = true)
+        {
+            if (strip) Strip();
+            clothes.SetParent(player, true);
+            clothesList.Add(clothes);
+        }
         /// <summary>
         /// Remove all clothes of the player
         /// </summary>
         public void Strip()
         {
-            foreach (ModelEntity c in clothes)
+            foreach (ModelEntity c in clothesList)
             {
                 c.Delete();
             }
-
-            clothes.Clear();
+            clothesList.Clear();
         }
     }
 }
