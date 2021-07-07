151085be0eb090c758906aaeb7339f675b762e9c
Lévy Marques <prakkmak@gmail.com>
2021-07-07 21:06:11 +0200

    feat: Quest class creation

---
+++ code/Quests/Quest.cs
@@ -0,0 +1,37 @@
+﻿namespace ssl.Quests
+{
+    /// <summary>
+    /// A quest is an objective for the player
+    /// </summary>
+    public abstract class Quest
+    {
+        /// <summary>
+        /// Name of the quest
+        /// </summary>
+        public abstract string Libelle { get; }
+        /// <summary>
+        /// Description of the question
+        /// </summary>
+        public abstract string Description { get; }
+        /// <summary>
+        /// When the quest is started
+        /// </summary>
+        public abstract void OnStart();
+        /// <summary>
+        /// Used when you want to check something each tick of the quest
+        /// </summary>
+        public abstract void OnUpdate();
+        /// <summary>
+        /// When the quest is completed with a success
+        /// </summary>
+        public abstract void OnCompete();
+        /// <summary>
+        /// When the quest is failed
+        /// </summary>
+        public abstract void OnFail();
+        /// <summary>
+        /// When the quest is canceled
+        /// </summary>
+        public abstract void OnCancel();
+    }
+}
