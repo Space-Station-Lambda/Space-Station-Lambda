131072275dbf209a2acc16d3b7dba95ffec429f2
Lévy Marques <prakkmak@gmail.com>
2021-07-11 11:25:02 +0200

    feat: BaseRound creation

--- code/Rounds/BaseRound.cs
+++ code/Rounds/BaseRound.cs
@@ -1,9 +1,86 @@
-﻿using Sandbox;
+﻿using System.Collections.Generic;
+using Sandbox;
+using ssl.Player;
 
 namespace ssl.Rounds
 {
-    public class BaseRound : Networked
+    public abstract partial class BaseRound : Networked
     {
-        
+        public virtual int RoundDuration => 0;
+        public virtual string RoundName => "";
+        public HashSet<MainPlayer> Players = new();
+        public float RoundEndTime { get; set; }
+        public float TimeLeft => RoundEndTime - Time.Now;
+
+        public void Start()
+        {
+            if (Host.IsServer && RoundDuration > 0)
+            {
+                RoundEndTime = Time.Now + RoundDuration;
+            }
+
+            OnStart();
+        }
+
+        public void Finish()
+        {
+            if (Host.IsServer)
+            {
+                RoundEndTime = 0f;
+                Players.Clear();
+            }
+
+            OnFinish();
+        }
+
+        public void AddPlayer(MainPlayer player)
+        {
+            Host.AssertServer();
+            if (!Players.Contains(player))
+            {
+                Players.Add(player);
+            }
+        }
+
+        public virtual void OnPlayerSpawn(MainPlayer player)
+        {
+        }
+
+        public virtual void OnPlayerKilled(MainPlayer player)
+        {
+        }
+
+        public virtual void OnPlayerLeave(MainPlayer player)
+        {
+            Players.Remove(player);
+        }
+
+        public virtual void OnTick()
+        {
+        }
+
+        public virtual void OnSecond()
+        {
+            if (Host.IsServer)
+            {
+                if (RoundEndTime > 0 && Time.Now >= RoundEndTime)
+                {
+                    RoundEndTime = 0f;
+                    OnTimeUp();
+                }
+            }
+        }
+
+        protected virtual void OnStart()
+        {
+        }
+
+        protected virtual void OnFinish()
+        {
+        }
+
+        protected virtual void OnTimeUp()
+        {
+        }
     }
 }
