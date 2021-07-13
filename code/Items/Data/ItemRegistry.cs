a8cc84d0532aa0acae9be4c473c0cb031f0736c4
Noé Pécault <noe.pecault@etu.univ-tours.fr>
2021-07-13 15:25:00 +0200

    feat: Added ItemRegistry

---
+++ code/Items/Data/ItemRegistry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ssl.Items.Data
+{
+	/// <summary>
+	/// Stores all references of item archetypes.
+	/// </summary>
+	public class ItemRegistry
+	{
+		private readonly Dictionary<string, Item> registry;
+
+		public ItemRegistry()
+		{
+			registry = new Dictionary<string, Item>();
+			
+			RegisterItems();
+		}
+
+		public Item Apple => registry["item.food.apple"]; //Item shortcut example
+		
+		public void Add(Item item)
+		{
+			registry.Add(item.Id, item);
+		}
+
+		public Item GetItemById(string id)
+		{
+			if (!registry.ContainsKey(id))
+			{
+				throw new NotImplementedException( "This item does not exist.");
+			}
+
+			return registry[id];
+		}
+
+		/// <summary>
+		/// Register all the game items in the registry.
+		/// In the future, this should disappear and should load items from an XML / JSON file.
+		/// </summary>
+		private void RegisterItems()
+		{
+			Add(new ItemFood("item.food.apple", "Apple", "", 1, 10)); //Item registration Example
+		}
+	}
+}
