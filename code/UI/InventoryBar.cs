a64db08e7c508631e2fab113d66847e8590d539f
Noé Pécault <noepec1@gmail.com>
2021-07-18 21:23:23 +0200

    feat: Added Scene for each Item slot

--- code/UI/InventoryBar.cs
+++ code/UI/InventoryBar.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Sandbox;
 using Sandbox.UI;
 using Sandbox.UI.Construct;
+using ssl.Items;
+using ssl.Items.Data;
 using ssl.Player;
 
 namespace ssl.UI
@@ -30,6 +33,7 @@ namespace ssl.UI
             if (selected < 0) selected = 9;
             if (selected > 9) selected = 0;
             icons[selected].SetClass("selected", true);
+            icons[selected].RefreshModel();
             ConsoleSystem.Run("set_inventory_holding", selected);
         }
 
@@ -38,7 +42,6 @@ namespace ssl.UI
         {
             if (Local.Pawn is not MainPlayer player)
                 return;
-
 
             if (input.Pressed(InputButton.Slot1)) SelectSlot(1);
             if (input.Pressed(InputButton.Slot2)) SelectSlot(2);
@@ -53,17 +56,56 @@ namespace ssl.UI
 
             if (input.MouseWheel != 0) SelectSlot(selected + input.MouseWheel);
         }
-
+        
         public class InventoryIcon : Panel
         {
+            private Scene scene;
+            private SceneWorld sceneWorld;
+            private SceneObject sceneObject;
+            private Light sceneLight;
+            
             public InventoryIcon(int slotNumber, Panel parent)
             {
                 SlotNumber = slotNumber;
                 Parent = parent;
                 Add.Label($"{SlotNumber}");
+                RefreshModel();
             }
 
             public int SlotNumber { get; private set; }
+
+            public void RefreshModel()
+            {
+                if (Local.Client.Pawn is not MainPlayer)
+                    return;
+                
+                MainPlayer player = (MainPlayer) Local.Client.Pawn;
+                
+                sceneWorld = new SceneWorld();
+                Log.Info(player.Inventory.SlotsFull);
+
+                using (SceneWorld.SetCurrent(sceneWorld))
+                {
+                    if (!player.Inventory.IsSlotEmpty(SlotNumber))
+                    {
+                        Model model = Model.Load("models/knife/knife.vmdl");
+                        sceneObject = new SceneObject(model, Transform.Zero);
+                        sceneLight = Light.Point(Vector3.Up * 10.0f + Vector3.Forward * 100.0f - Vector3.Right * 100.0f,
+                            2000, Color.White * 15000.0f);
+                    }
+                }
+            
+                Angles angles = new(30, 180+45, 0);
+                Vector3 pos = new(10, 10, 10);
+                if (scene != null)
+                {
+                    scene.World = sceneWorld;
+                }
+                else
+                {
+                    scene = Add.Scene(sceneWorld, pos, angles, 50, "itemslot-model");
+                }
+            }
         }
     }
 }
