c04eeb9a8974203d89547af8169c1554c314151c
Lévy MARQUES <Prakkmak@gmail.com>
2021-07-19 18:32:21 +0200

    Update code/Rounds/PreRound.cs

--- code/Rounds/PreRound.cs
+++ code/Rounds/PreRound.cs
@@ -21,9 +21,8 @@ namespace ssl.Rounds
 
         protected override void OnTimeUp()
         {
-            Log.Info("Times up");
-            AssignRoles();
             base.OnTimeUp();
+            AssignRoles();
         }
 
         private void AssignRoles()
