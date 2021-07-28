2ad2e078a0263f2b8cefcc34c2e1bba1ff5098cd
André <andremgmonteiro01@gmail.com>
2021-07-28 21:47:31 +0100

    fix: Changed Bugged Clothes
    Pearl Earings were not showing up in game and heel shoes had a huge size. Removed both and added simple tennis.

--- code/Player/Roles/Assistant.cs
+++ code/Player/Roles/Assistant.cs
@@ -12,8 +12,7 @@ namespace ssl.Player.Roles
         {
             "models/citizen_clothes/hair/hair_femalebun.blonde.vmdl",
             "models/citizen_clothes/dress/dress.kneelength.vmdl",
-            "models/citizen_clothes/accessory/pearlearrings.vmdl",
-            "models/citizen_clothes/shoes/shoes_heels.vmdl"
+            "models/citizen_clothes/shoes/trainers.vmdl_c"
         };
     }
 }
