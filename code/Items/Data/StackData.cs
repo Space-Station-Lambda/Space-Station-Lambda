5e977c568190cd239328995e4ea7a73c19587f3b
Noé Pécault <noepec1@gmail.com>
2021-08-04 00:51:55 +0200

    docs: Added documentation on StackData

--- code/Items/Data/StackData.cs
+++ code/Items/Data/StackData.cs
@@ -1,5 +1,8 @@
 ﻿namespace ssl.Items
 {
+    /// <summary>
+    /// Custom data of an ItemStack. Should be inherited to add the properties.
+    /// </summary>
     public abstract class StackData : NetworkedEntityAlwaysTransmitted
     {
     }
