aa19cd442dd1546949c9bcb53f1fc67c0abcbe47
Lévy Marques <prakkmak@gmail.com>
2021-08-15 11:03:19 +0200

    feat: Selector classes

---
+++ code/Modules/Selection/ISelectable.cs
@@ -0,0 +1,26 @@
+﻿namespace ssl.Player
+{
+    public interface ISelectable
+    {
+        /// <summary>
+        /// When the selection started.
+        /// </summary>
+        /// <param name="player">The player who select.</param>
+        public void OnSelectStart(ISelector player);
+        /// <summary>
+        /// When the slection stopped.
+        /// </summary>
+        /// <param name="player">The player who select.</param>
+        public void OnSelectStop(ISelector player);
+        /// <summary>
+        /// While the is selected.
+        /// </summary>
+        /// <param name="player">The player who select.</param>
+        public void OnSelect(ISelector player);
+        /// <summary>
+        /// Trigger when the action is requested by the player
+        /// </summary>
+        /// <param name="player">The player who perform the action.</param>
+        public void OnAction(ISelector player);
+    }
+}
---
+++ code/Modules/Selection/PlayerSelector.cs
@@ -0,0 +1,65 @@
+﻿using Sandbox;
+
+namespace ssl.Player
+{
+    public class PlayerSelector : ISelector
+    {
+        private const float SelectionRange = 1000f;
+        
+        private MainPlayer player;
+        private ISelectable selected;
+        
+        
+        public PlayerSelector(MainPlayer player)
+        {
+            this.player = player;
+        }
+
+        public void CheckSelection()
+        {
+            Vector3 forward = player.EyeRot.Forward;
+            TraceResult tr = TraceSelector(player.EyePos, player.EyePos + forward * SelectionRange);
+            Entity result = tr.Entity;
+            if (result is ISelectable selectable)
+            {
+                if (selected != selectable)
+                {
+                    StopSelection();
+                    StartSelection(selectable);
+                }
+                selected.OnSelect(this);
+            }
+            else
+            {
+                StopSelection();
+            }
+                
+        }
+
+        public void StartSelection(ISelectable selectable)
+        {
+            selected.OnSelectStop(this);
+            selected = null;
+        }
+        
+        public void StopSelection()
+        {
+            selected.OnSelectStop(this);
+            selected = null;
+        }
+        
+        
+        protected virtual TraceResult TraceSelector(Vector3 start, Vector3 end)
+        {
+            bool inWater = Physics.TestPointContents(start, CollisionLayer.Water);
+
+            TraceResult tr = Trace.Ray(start, end)
+                .UseHitboxes()
+                .HitLayer(CollisionLayer.Water, !inWater)
+                .Ignore(player)
+                .Run();
+
+            return tr;
+        }
+    }
+}
