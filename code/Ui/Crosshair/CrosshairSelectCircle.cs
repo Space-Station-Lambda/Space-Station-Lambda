c0531b4a91ba03fdbda89d8016f33a43b801047b
Lévy Marques <prakkmak@gmail.com>
2021-08-15 20:37:51 +0200

    feat: add selectcircle

---
+++ code/Ui/Crosshair/CrosshairSelectCircle.cs
@@ -0,0 +1,23 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using ssl.Player;
+
+namespace ssl.UI
+{
+    public class CrosshairSelectionCircle : Panel
+    {
+        public CrosshairSelectionCircle()
+        {
+            StyleSheet.Load("ui/CrosshairSelectionCircle.scss");
+            SetClass("selected", false);
+        }
+            
+        public override void Tick()
+        {
+            if (Local.Pawn is not MainPlayer player)
+                return;
+
+            SetClass("selected", player.Selector.IsSelected());
+        }
+    }
+}
