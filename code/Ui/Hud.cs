2d066d1853a4f4bba6e8a3c34d08bdebf8f2dd16
Lévy Marques <prakkmak@gmail.com>
2021-08-15 20:38:51 +0200

    fix: hud

---
+++ code/Ui/Hud.cs
@@ -0,0 +1,18 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace ssl.UI
+{
+    [Library]
+    public partial class Hud : HudEntity<RootPanel>
+    {
+        public Hud()
+        {
+            if (!IsClient) return;
+            RootPanel.AddChild<RoundInfos>();
+            RootPanel.AddChild<InventoryBar>();
+            RootPanel.AddChild<Crosshair>();
+            RootPanel.AddChild<RoleSelector>();
+        }
+    }
+}
