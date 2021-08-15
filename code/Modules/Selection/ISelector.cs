80960ad96ab67c79e34056e37eafb4771cc3363d
Lévy Marques <prakkmak@gmail.com>
2021-08-15 11:03:19 +0200

    feat: Selector classes

---
+++ code/Modules/Selection/ISelector.cs
@@ -0,0 +1,10 @@
+﻿using Sandbox;
+
+namespace ssl.Player
+{
+    public interface ISelector
+    {
+        public void StopSelection();
+        public void StartSelection(ISelectable selectable);
+    }
+}
--- code/Player/MainPlayer.cs
+++ code/Player/MainPlayer.cs
@@ -9,7 +9,7 @@ using SpawnPoint = ssl.Modules.Rounds.SpawnPoint;
 
 namespace ssl.Player
 {
-    public partial class MainPlayer : Sandbox.Player
+    public partial class MainPlayer : Sandbox.Player, ISelectable
     {
         private const string Model = "models/citizen/citizen.vmdl";
         private const int MaxInventoryCapacity = 10;
@@ -164,6 +164,26 @@ namespace ssl.Player
             ragdoll.Player = this;
 
             Ragdoll = ragdoll;
+        }
+
+        public void OnSelectStart(ISelector player)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnSelectStop(ISelector player)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnSelect(ISelector player)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnAction(ISelector player)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
