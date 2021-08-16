584cd1e47781fc235a8c83c3c4a73097a095c1a8
Lévy Marques <prakkmak@gmail.com>
2021-08-16 19:45:24 +0200

    fix: merge

--- code/Player/MainPlayer.cs
+++ code/Player/MainPlayer.cs
@@ -14,7 +14,6 @@ namespace ssl.Player
         private const string Model = "models/citizen/citizen.vmdl";
         private const int MaxInventoryCapacity = 10;
         private const float MaxHealth = 100f;
-        private ISelector Selector; 
 
         public MainPlayer()
         {
