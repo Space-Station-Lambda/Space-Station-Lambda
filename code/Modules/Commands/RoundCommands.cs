1a55626c1208b6ae2a4dd106dda2fdf44b57c1c6
Lévy Marques <prakkmak@gmail.com>
2021-08-16 22:14:50 +0200

    feat: round commands

---
+++ code/Modules/Commands/RoundCommands.cs
@@ -0,0 +1,31 @@
+﻿using Sandbox;
+
+namespace ssl.Modules.Commands
+{
+    public class RoundCommands
+    {
+        [AdminCmd("rrestart")]
+        public static void RestartRound()
+        {
+            Gamemode.Instance.RoundManager.ChangeRound(Gamemode.Instance.RoundManager.CurrentRound);
+        }
+
+        [AdminCmd("rend")]
+        public static void RoundEnd()
+        {
+            Gamemode.Instance.RoundManager.CurrentRound.Finish();
+        }
+        
+        [AdminCmd("rset")]
+        public static void RoundSetTimer(int timer)
+        {
+            Gamemode.Instance.RoundManager.CurrentRound.RoundEndTime = Time.Now + timer;
+        }
+        
+        [AdminCmd("rtime")]
+        public static void RoundGetTimer()
+        {
+            Log.Info(Gamemode.Instance.RoundManager.CurrentRound.TimeLeftFormatted);
+        }
+    }
+}
