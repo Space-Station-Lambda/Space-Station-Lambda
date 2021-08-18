21125b62f080127f8d2f17643f620b63d500ebf8
Lévy Marques <prakkmak@gmail.com>
2021-08-18 18:50:34 +0200

    feat: change typo

--- code/Modules/Commands/RoundCommands.cs
+++ code/Modules/Commands/RoundCommands.cs
@@ -4,25 +4,25 @@ namespace ssl.Modules.Commands
 {
     public class RoundCommands
     {
-        [AdminCmd("rrestart")]
+        [AdminCmd("rn_start")]
         public static void RestartRound()
         {
             Gamemode.Instance.RoundManager.ChangeRound(Gamemode.Instance.RoundManager.CurrentRound);
         }
 
-        [AdminCmd("rend")]
+        [AdminCmd("rn_end")]
         public static void RoundEnd()
         {
             Gamemode.Instance.RoundManager.CurrentRound.Finish();
         }
         
-        [AdminCmd("rset")]
+        [AdminCmd("rn_set")]
         public static void RoundSetTimer(int timer)
         {
             Gamemode.Instance.RoundManager.CurrentRound.RoundEndTime = Time.Now + timer;
         }
         
-        [AdminCmd("rtime")]
+        [AdminCmd("rn_time")]
         public static void RoundGetTimer()
         {
             Log.Info(Gamemode.Instance.RoundManager.CurrentRound.TimeLeftFormatted);
