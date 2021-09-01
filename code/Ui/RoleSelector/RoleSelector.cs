af4d9632f37049d6e13444bab5726a979cc88705
Noé Pécault <noepec1@gmail.com>
2021-09-02 01:08:47 +0200

    refactor: Removed unused methods

--- code/Ui/RoleSelector/RoleSelector.cs
+++ code/Ui/RoleSelector/RoleSelector.cs
@@ -50,20 +50,6 @@ namespace ssl.Ui.RoleSelector
             }
         }
 
-        private void OnRoundStarted()
-        {
-            Log.Trace("[RoleSelector] Round started");
-            BaseRound currentRound = Gamemode.Instance.RoundManager.CurrentRound;
-            SetClass("active", currentRound is PreRound);
-            SetClass("hidden", currentRound is not PreRound);
-        }
-
-        private void OnRoundManagerCreated()
-        {
-            Log.Trace("[RoleSelector] Round created");
-            //Gamemode.Instance.RoundManager.RoundStarted += OnRoundStarted;
-        }
-
         public override void Tick()
         {
             base.Tick();
