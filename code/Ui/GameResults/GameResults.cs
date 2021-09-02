b56d284b273825e64cdf3a6032c20cdab8472b5f
Noé Pécault <noepec1@gmail.com>
2021-09-02 15:42:39 +0200

    feat: Basic GameResults screen

---
+++ code/Ui/GameResults/GameResults.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using ssl.Modules.Rounds;
+
+namespace ssl.Ui.GameResults
+{
+    public class GameResults : Panel
+    {
+        private Label winningTeam;
+        
+        public GameResults()
+        {
+            StyleSheet.Load("Ui/GameResults/GameResults.scss");
+
+            winningTeam = Add.Label(classname: "winning-team");
+            
+            SetClass("active", false);
+            SetClass("hidden", true);
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+            
+            ResultsRound currentRound = Gamemode.Instance.RoundManager?.CurrentRound as ResultsRound;
+            
+            SetClass("active", null != currentRound);
+            SetClass("hidden", null == currentRound);
+
+            if (currentRound != null)
+            {
+                winningTeam.SetText("HGELO");
+            }
+        }
+    }
+}
