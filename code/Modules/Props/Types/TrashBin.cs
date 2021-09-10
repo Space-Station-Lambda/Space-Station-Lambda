edcb6c145647ab8c3af47f76c4acb725de620237
Lévy Marques <prakkmak@gmail.com>
2021-09-10 18:30:24 +0200

    feat(janitor): janitor props

---
+++ code/Modules/Props/Types/TrashBin.cs
@@ -0,0 +1,17 @@
+﻿using ssl.Modules.Props.Data;
+
+namespace ssl.Modules.Props.Types
+{
+    public class TrashBin : Prop
+    {
+        private const int MaxTrash = 50;
+        
+        public TrashBin()
+        {
+        }
+
+        public TrashBin(PropData propData) : base(propData)
+        {
+        }
+    }
+}
