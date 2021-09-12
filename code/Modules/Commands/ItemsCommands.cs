7c2942bf35c3480dfc89c9643b1dfc06f42c5ee7
Lévy Marques <prakkmak@gmail.com>
2021-09-12 12:35:51 +0200

    feat: Items commands

---
+++ code/Modules/Commands/ItemsCommands.cs
@@ -0,0 +1,41 @@
+﻿using Sandbox;
+using ssl.Modules.Items;
+using ssl.Modules.Items.Carriables;
+using ssl.Player;
+
+namespace ssl.Modules.Commands
+{
+    public class ItemsCommands
+    {
+        /// <summary>
+        /// Clear the inventory
+        /// </summary>
+        [AdminCmd("clear")]
+        public static void ClearInventory()
+        {
+            Client client = ConsoleSystem.Caller;
+            MainPlayer player = (MainPlayer)client.Pawn;
+            player.Inventory.Clear();
+            Log.Info("Your inventory is now clear.");
+        }
+        /// <summary>
+        /// give an item
+        /// </summary>
+        [AdminCmd("give")]
+        public static void GiveItem(string id)
+        {
+            Client client = ConsoleSystem.Caller;
+            MainPlayer player = (MainPlayer)client.Pawn;
+            ItemFactory itemFactory = new();
+            try
+            {
+                Item item = itemFactory.Create(id);
+                player.Inventory.Add(item);
+            }
+            catch
+            {
+                Log.Info($"{id} not found.");
+            }
+        }
+    }
+}
