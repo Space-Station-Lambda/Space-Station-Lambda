08337f7ab3f4fc9dbd88133c4dabd2cf08ebaeaa
Noé Pécault <noepec1@gmail.com>
2021-09-17 13:05:09 +0200

    refactor: Cycle target methods
    refactor: Change FOV on Start
    
    refactor: Renamed up variable

--- code/Player/Cameras/SpectatorCamera.cs
+++ code/Player/Cameras/SpectatorCamera.cs
@@ -11,13 +11,15 @@ namespace ssl.Player.Cameras
 
         private int playerIndex;
 
-        private List<MainPlayer> players => Gamemode.Instance.RoundManager.CurrentRound.Players;
+        private List<MainPlayer> Players => Gamemode.Instance.RoundManager.CurrentRound.Players;
         [Net, Predicted] public float MoveSpeed { get; private set; } = 50F;
         [Net, Predicted] public Entity Target { get; private set; }
         
         public override void Activated()
         {
             base.Activated();
+            
+            FieldOfView = SpectatorFieldOfView;
         }
 
         public override void Update()
@@ -29,36 +31,43 @@ namespace ssl.Player.Cameras
             }
             else
             {
-                float upVector = 0;
+                float up = 0;
                 if (Input.Pressed(InputButton.Jump))
                 {
-                    upVector = 1;
+                    up = 1;
                 }
                 else if (Input.Pressed(InputButton.Duck))
                 {
-                    upVector = -1;
+                    up = -1;
                 }
 
-                Vector3 wishDir = new Vector3(Input.Forward, Input.Left, upVector).Normal * Rot;
+                Vector3 wishDir = new Vector3(Input.Forward, Input.Left, up).Normal * Rot;
                 Pos += wishDir * MoveSpeed;
                 Rot = Input.Rotation;
             }
 
-            int playerCount = Gamemode.Instance.RoundManager.CurrentRound.Players.Count;
-            
             if (Input.Pressed(InputButton.Attack1))
-            {
-                playerIndex = playerIndex - 1 < 0 ? playerCount - 1 : playerIndex - 1;
-            }
+                PreviousTarget();
             else if (Input.Pressed(InputButton.Attack2))
-            {
-                playerIndex = (playerIndex + 1) % playerCount;
-            }
+                NextTarget();
+        }
 
-            if (0 <= playerIndex && playerIndex < playerCount) 
-                Target = players[playerIndex];
+        private void NextTarget()
+        {
+            playerIndex = (playerIndex + 1) % Players.Count;
+            UpdateTarget();
+        }
 
-            FieldOfView = SpectatorFieldOfView;
+        private void PreviousTarget()
+        {
+            playerIndex = playerIndex - 1 < 0 ? Players.Count - 1 : playerIndex - 1;
+            UpdateTarget();
+        }
+
+        private void UpdateTarget()
+        {
+            if (0 <= playerIndex && playerIndex < Players.Count) 
+                Target = Players[playerIndex];
         }
     }
 }
