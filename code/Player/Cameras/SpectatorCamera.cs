80171aea2bb30fd29e03d5789aa45fa1824e7bd8
Noé Pécault <noepec1@gmail.com>
2021-09-17 15:42:44 +0200

    fix: Release target camera fixed
    fix: NextTarget divide by zero
    
    fix: Up direction not rotated

--- code/Player/Cameras/SpectatorCamera.cs
+++ code/Player/Cameras/SpectatorCamera.cs
@@ -8,17 +8,21 @@ namespace ssl.Player.Cameras
     {
         private const float SpectatorFieldOfView = 80F;
         private const float FocusDistance = 100F;
+        private const float MaxSpeed = 75F;
 
+        private bool clearTargetPressed;
         private int playerIndex;
 
-        private List<MainPlayer> Players => Gamemode.Instance.RoundManager.CurrentRound.Players;
-        [Net, Predicted] public float MoveSpeed { get; private set; } = 50F;
-        [Net, Predicted] public Entity Target { get; private set; }
-        
+        private IReadOnlyList<Entity> Players => Gamemode.Instance.RoundManager.CurrentRound.Players;
+        public float MoveSpeed { get; private set; } = 50F;
+        public Entity Target { get; private set; }
+
+
         public override void Activated()
         {
             base.Activated();
-            
+
+            Target = null;
             FieldOfView = SpectatorFieldOfView;
         }
 
@@ -27,34 +31,58 @@ namespace ssl.Player.Cameras
             if (Target != null)
             {
                 Pos = Target.Position + Input.Rotation.Backward * FocusDistance;
-                Rot = Target.EyeRot;
+                Rot = Input.Rotation;
+
+                if (Input.Pressed(InputButton.Jump))
+                {
+                    Target = null;
+                    clearTargetPressed = true;
+                }
+
+                MoveSpeed += Input.MouseWheel;
+                MoveSpeed.Clamp(0, MaxSpeed);
             }
             else
             {
                 float up = 0;
-                if (Input.Pressed(InputButton.Jump))
-                {
-                    up = 1;
-                }
-                else if (Input.Pressed(InputButton.Duck))
-                {
-                    up = -1;
-                }
 
-                Vector3 wishDir = new Vector3(Input.Forward, Input.Left, up).Normal * Rot;
+                if (!clearTargetPressed)
+                {
+                    if (Input.Down(InputButton.Jump))
+                    {
+                        up = 1;
+                    }
+                    else if (Input.Down(InputButton.Duck))
+                    {
+                        up = -1;
+                    }
+                } 
+                else if (Input.Released(InputButton.Jump))
+                {
+                    clearTargetPressed = false;
+                }
+                
+                Vector3 wishDir = new Vector3(Input.Forward, Input.Left, 0).Normal * Rot;
+                wishDir += Vector3.Up * up;
+                
                 Pos += wishDir * MoveSpeed;
                 Rot = Input.Rotation;
             }
 
             if (Input.Pressed(InputButton.Attack1))
+            {
                 PreviousTarget();
+            }
             else if (Input.Pressed(InputButton.Attack2))
+            {
                 NextTarget();
+            }
         }
-
+        
         private void NextTarget()
         {
-            playerIndex = (playerIndex + 1) % Players.Count;
+
+            playerIndex = Players.Count > 0 ? (playerIndex + 1) % Players.Count : -1;
             UpdateTarget();
         }
 
@@ -66,8 +94,11 @@ namespace ssl.Player.Cameras
 
         private void UpdateTarget()
         {
-            if (0 <= playerIndex && playerIndex < Players.Count) 
+            if (0 <= playerIndex && playerIndex < Players.Count)
+            {
                 Target = Players[playerIndex];
+                Log.Trace(Target);
+            }
         }
     }
 }
