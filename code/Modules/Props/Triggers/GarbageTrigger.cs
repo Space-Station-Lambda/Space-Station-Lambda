89a9dc7f8daba26378160f8bcf51b9b82d458def
Noé Pécault <noepec1@gmail.com>
2021-09-23 09:22:38 +0200

    feat(garbage): Added GarbageTrigger

---
+++ code/Modules/Props/Triggers/GarbageTrigger.cs
@@ -0,0 +1,13 @@
+﻿using Sandbox;
+
+namespace ssl.Modules.Props.Triggers
+{
+    public class GarbageTrigger : BaseTrigger
+    {
+        public override void OnTouchStart(Entity toucher)
+        {
+            base.OnTouchStart(toucher);
+            toucher.Delete();
+        }
+    }
+}
