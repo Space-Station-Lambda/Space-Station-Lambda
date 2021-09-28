16242d3f14afe0b977c0a3c84c1182e8646aea52
Noé Pécault <noepec1@gmail.com>
2021-09-28 23:36:41 +0200

    feat(dragger): Added Dragger and IDraggable

---
+++ code/Modules/Selection/Dragger.cs
@@ -0,0 +1,45 @@
+﻿using Sandbox;
+using ssl.Player;
+
+namespace ssl.Modules.Selection
+{
+    public class Dragger : Selector
+    {
+        public Dragger(MainPlayer player) : base(player)
+        {
+        }
+        
+        public IDraggable Dragged { get; private set; }
+
+        public override void UpdateTarget()
+        {
+            base.UpdateTarget();
+            
+            Entity result = GetTraceResultEntity();
+            if (result is IDraggable draggable)
+            {
+                if (!ReferenceEquals(draggable, Dragged))
+                {
+                    StopDrag();
+                    StartDrag(draggable);
+                }
+
+                Dragged.OnDrag(player);
+            }
+            else if (Selected != null)
+            {
+                StopDrag();
+            }
+        }
+
+        private void StartDrag(IDraggable draggable)
+        {
+            //TODO: Create joint
+        }
+
+        private void StopDrag()
+        {
+            //TODO: Clear joint
+        }
+    }
+}
---
+++ code/Modules/Selection/IDraggable.cs
@@ -0,0 +1,31 @@
+﻿using ssl.Player;
+
+namespace ssl.Modules.Selection
+{
+    public interface IDraggable
+    {
+        /// <summary>
+        /// When the drag started.
+        /// </summary>
+        /// <param name="player">The player who drags.</param>
+        void OnDragStart(MainPlayer player);
+
+        /// <summary>
+        /// When the drag stopped.
+        /// </summary>
+        /// <param name="player">The player who drags.</param>
+        void OnDragStop(MainPlayer player);
+
+        /// <summary>
+        /// While the entity is dragged.
+        /// </summary>
+        /// <param name="player">The player who drags.</param>
+        void OnDrag(MainPlayer player);
+
+        /// <summary>
+        /// Check if the Draggable can be dragged by the player
+        /// </summary>
+        /// <param name="player">The player who wants to drag.</param>
+        bool IsDraggable(MainPlayer player);
+    }
+}
