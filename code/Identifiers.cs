044cdb5126e9d566db8c130188c94a2a77fdd6dc
Noé Pécault <noepec1@gmail.com>
2021-12-05 22:29:48 +0100

    refactor(factory): Refactored Item factory

--- code/Identifiers.cs
+++ code/Identifiers.cs
@@ -3,8 +3,17 @@ namespace ssl;
 public static class Identifiers
 {
 	public const string Separator = ".";
+	
+	#region Prop
 	public const string Bucket = "bucket";
 	public const string Prop = "prop";
 	public const string TrashBin = "trash_bin";
 	public const string Machine = "machine";
+	#endregion
+	
+	#region Item
+	public const string Food = "Food";
+	public const string Weapon = "Weapon";
+	public const string Cleaner = "Cleaner";
+	#endregion
 }
