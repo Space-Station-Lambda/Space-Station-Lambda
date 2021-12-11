b5fc8f26535b0fd888b2908676be0c84f9d7f414
Noé Pécault <noepec1@gmail.com>
2021-12-11 22:23:41 +0100

    feat(status): Added status DAO

---
+++ code/Modules/Statuses/StatusDao.cs
@@ -0,0 +1,25 @@
+﻿using ssl.Commons;
+
+namespace ssl.Modules.Statuses;
+
+public class StatusDao : LocalDao<StatusData>
+{
+	private static StatusDao instance;
+
+	private StatusDao()
+	{
+	}
+	
+	public static StatusDao Instance => instance ??= new StatusDao();
+
+	/// <summary>
+	///     Load all data from disk files.
+	/// </summary>
+	protected override void LoadAll()
+	{
+		Log.Info("Load items..");
+
+		// Statuses
+		Log.Info($"{All.Count} items charged !");
+	}
+}
