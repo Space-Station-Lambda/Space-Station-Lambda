c8e300f482a768f2afe05b75df6acf7df1c02b6e
Noé Pécault <noepec1@gmail.com>
2022-02-06 20:32:49 +0100

    feat: Added Bistable doors with Airlock

---
+++ code/Modules/Props/Instances/DoorBistable.cs
@@ -0,0 +1,48 @@
+using Sandbox;
+using ssl.Player;
+
+namespace ssl.Modules.Props.Instances;
+
+/// <summary>
+/// Simple bistable door that can can switch between two states (Open and Closed).
+///
+/// The AnimGraph of the model needs to have a boolean parameter "opened" that represents in which state the door should be in. 
+/// </summary>
+[Library("ssl_door_bistable")]
+public class DoorBistable : Prop
+{
+    private const float INTERACTION_DELAY = 1F;
+    private TimeSince lastUsed;
+    
+    public bool Opened { get; set; }
+
+    public override void OnInteract(SslPlayer sslPlayer, int strength)
+    {
+        if (!(lastUsed > INTERACTION_DELAY)) return;
+        
+        if (Opened)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+
+        lastUsed = 0;
+    }
+
+    [Input]
+    private void Open()
+    {
+        SetAnimBool("opened", true);
+        Opened = true;
+    }
+
+    [Input]
+    private void Close()
+    {
+        SetAnimBool("opened", false);
+        Opened = false;
+    }
+}
