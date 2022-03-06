452394505204544434af00bff5e8dcdf50d0ab94
Noé Pécault <noepec1@gmail.com>
2022-03-06 23:38:44 +0100

    feat: Converted previous FP arms workaround

--- code/Player/HandViewModel.cs
+++ code/Player/HandViewModel.cs
@@ -8,31 +8,18 @@ namespace ssl.Player;
 /// </summary>
 public class HandViewModel : AnimEntity
 {
-    private const string VIEW_MODEL_PATH = "models/players/citizen/v_citizen.vmdl";
-    private const string ANIM_KEY_HOLD_TYPE = "holdtype";
-    private const string ANIM_KEY_BODY_WEIGHT = "aim_body_weight";
-    private const string BODY_GROUP_HEAD = "head";
-    private const string BODY_GROUP_LEGS = "legs";
-    private const string BODY_GROUP_FEET = "feet";
-
-    private Vector3 offset;
-
+    private const string VIEW_MODEL_PATH = "models/players/citizen/first_person_arms.vmdl";
+    
     public HandViewModel()
     {
         Host.AssertClient();
 
         SetModel(VIEW_MODEL_PATH);
-        SetBodyGroup(BODY_GROUP_HEAD, 1);
-        SetBodyGroup(BODY_GROUP_LEGS, 1);
-        SetBodyGroup(BODY_GROUP_FEET, 1);
-        SetAnimParameter(ANIM_KEY_BODY_WEIGHT, 0f);
         RemoveHoldingEntity();
     }
 
     public AnimEntity HoldingEntity { get; private set; }
 
-    private static Vector3 HandOffset { get; } = -(Vector3.Up * 50 + Vector3.Right);
-    private static Vector3 PistolOffset { get; } = -(Vector3.Up * 60 + Vector3.Right);
 
     /// <summary>
     ///     Modify the holdtype of the animgraph and the offset of the viewmodel?
@@ -40,19 +27,26 @@ public class HandViewModel : AnimEntity
     /// <exception cref="ArgumentOutOfRangeException"></exception>
     public void SetHoldType(HoldType holdType)
     {
-        SetAnimParameter(ANIM_KEY_HOLD_TYPE, (int) holdType);
-        offset = holdType switch
+        EnableDrawing = true;
+        switch (holdType)
         {
-            HoldType.None => offset,
-            HoldType.Pistol => PistolOffset,
-            HoldType.Hand => HandOffset,
-            _ => throw new ArgumentOutOfRangeException(nameof(holdType), holdType, null)
-        };
+            case HoldType.None:
+                EnableDrawing = false;
+                break;
+            case HoldType.Pistol:
+                SetAnimParameter("FingerAdjustment_BlendNeutralPose_R", 0);
+                break;
+            case HoldType.Hand:
+                SetAnimParameter("FingerAdjustment_BlendNeutralPose_R", 1);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(holdType), holdType, null);
+        }
     }
 
     public override void PostCameraSetup(ref CameraSetup camSetup)
     {
-        Position = camSetup.Position + offset * Input.Rotation;
+        Position = camSetup.Position;
         Rotation = camSetup.Rotation;
     }
 
@@ -64,8 +58,12 @@ public class HandViewModel : AnimEntity
     {
         Host.AssertClient();
         HoldingEntity?.Delete();
-        HoldingEntity = new AnimEntity { UsePhysicsCollision = false, EnableViewmodelRendering = true, Owner = this };
-        HoldingEntity.Model = entity.Model;
+        HoldingEntity = new AnimEntity { 
+            UsePhysicsCollision = false, 
+            EnableViewmodelRendering = true, 
+            Owner = this,
+            Model = entity.Model
+        };
         HoldingEntity.SetParent(this, true);
     }
 
