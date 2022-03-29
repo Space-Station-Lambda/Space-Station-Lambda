cd2c4307b01dba7c96763eed11b5e31999a609e3
Noé Pécault <noepec1@gmail.com>
2022-03-29 04:16:17 +0200

    feat: UsePrimary of clothing

--- code/Modules/Items/Instances/ItemClothes.cs
+++ code/Modules/Items/Instances/ItemClothes.cs
@@ -1,10 +1,22 @@
 ﻿using Sandbox;
 using ssl.Modules.Items.Instances;
+using ssl.Modules.Selection;
+using ssl.Player;
 
 namespace ssl.Modules.Clothes;
 
 public class ItemClothes : Item
 {
     public ClothesSlot Slot { get; set; }
-    
+    //TODO: Have real physics with model
+
+    public override void OnPressedUsePrimary(SslPlayer sslPlayer, ISelectable target)
+    {
+        base.OnPressedUsePrimary(sslPlayer, target);
+
+        Log.Info(sslPlayer.ClothesHandler);
+        
+        sslPlayer.Inventory.RemoveItem(this);
+        sslPlayer.ClothesHandler.AttachClothes(this);
+    }
 }
