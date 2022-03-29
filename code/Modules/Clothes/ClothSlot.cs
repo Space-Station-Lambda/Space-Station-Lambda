e38036a3b16c97bf5dc0d152ed796f32b2ed7ef9
Noé Pécault <noepec1@gmail.com>
2022-03-29 03:01:47 +0200

    feat: ClothSlot enum added

---
+++ code/Modules/Clothes/ClothSlot.cs
@@ -0,0 +1,18 @@
+namespace ssl.Modules.Clothes;
+
+/// <summary>
+///     Different places where a player can wear a piece of cloth.
+/// </summary>
+public enum ClothSlot
+{
+    Hat,
+    /// <summary>
+    ///     Clothes that are worn on the player's chest but under any vest.
+    /// </summary>
+    Shirt,
+    /// <summary>
+    ///     Clothes on the legs of the player.
+    /// </summary>
+    Pants,
+    Shoes
+}
